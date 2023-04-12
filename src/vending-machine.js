417bb091c6500eabdbe6584296dd76a181fadbfc
Rishabh <rishabhrawat74@users.noreply.github.com>
2023-04-12 07:32:06 +0530

    Added feature to dispense 2, 5 and 10 rupee coin

--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,5 +1,34 @@
-const vend = function (amount) {
+const coinOf10 = function (amount) {
+  return Math.floor(amount / 10);
+}
+
+const coinOf5 = function (amount) {
+  return Math.floor(amount / 5);
+}
+
+const coinOf2 = function (amount) {
+  return Math.floor(amount / 2);
+}
+
+const coinOf1 = function (amount) {
   return amount;
 }
 
-exports.vend = vend;
+const dispenseCoins = function (amount) {
+  let totalCoins = 0;
+
+  totalCoins += coinOf10(amount);
+  amount = amount % 10;
+
+  totalCoins += coinOf5(amount);
+  amount = amount % 5;
+
+  totalCoins += coinOf2(amount);
+  amount = amount % 2;
+
+  totalCoins += coinOf1(amount);
+
+  return totalCoins;
+}
+
+exports.vend = dispenseCoins;
--- test/vending-machine-test.js
+++ test/vending-machine-test.js
@@ -6,7 +6,11 @@ const toVend = function () {
 
   testing.assertEquals(values.vend(0), 0, "For amount 0 it should return 0 coins");
   testing.assertEquals(values.vend(1), 1, "For amount 1 it should return 1 coins");
-  testing.assertEquals(values.vend(5), 5, "For amount 5 it should return 5 coins");
+  testing.assertEquals(values.vend(2), 1, "For amount 2 it should return 1 coins");
+  testing.assertEquals(values.vend(5), 1, "For amount 5 it should return 1 coins");
+  testing.assertEquals(values.vend(10), 1, "For amount 10 it should return 1 coins");
+  testing.assertEquals(values.vend(18), 4, "For amount 18 it should return 4 coins");
+  testing.assertEquals(values.vend(21), 3, "For amount 21 it should return 3 coins");
 }
 
 toVend();
